c1ec9af392d5700487955a1c01ab912136af060e
xmickos <91094084+xmickos@users.noreply.github.com>
2022-09-29 22:37:48 +0300

    Update onegin.cpp
    goto oneginfuncs commit

--- onegin.cpp
+++ onegin.cpp
@@ -1,12 +1,8 @@
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <assert.h>
-#include <malloc.h> 
-#include <stat.h>
-#include <stdlib.h>
-#include <ctype.h>
 #include "onegin.hpp"
+
+// TODO: check includes – da
+// TODO: remove logging if not debug – da 
+// TODO: do not pass buffer by ptr – da
 
 int main(){
     int numofstrings_readed = 0;
@@ -15,18 +11,39 @@ int main(){
     long int readedcount = 0;
     char* buffer = nullptr;
     int numofstrings_computed = 0;
+
+    #ifdef NDEBUG
+
     FILE *logfile = fopen("logi.txt", "w");
 
+    buffer = readingfromfile(logfile, filereadname, &readedcount, &numofstrings_readed);
+
+
+    #else 
+
+    buffer = readingfromfile(nullptr, filereadname, &readedcount, &numofstrings_readed);
+
+    #endif
+
     //Ввод из файла: 
-    readingfromfile(logfile, filereadname, &buffer, &readedcount, &numofstrings_readed);
+
+    #ifdef NDEBUG
 
     if(buffer == nullptr){
         fprintf(logfile, "LINE %d:  (!) buffer is nullptr! buffer = %p\n", __LINE__, buffer);
         return -1;
     }
 
-    strinfo* strings_adressed = structsforming(logfile, &buffer, readedcount, numofstrings_readed, &numofstrings_computed);
-    
+    strinfo* strings_adressed = structsforming(logfile, buffer, readedcount, numofstrings_readed, &numofstrings_computed);
+
+    #else
+    strinfo* strings_adressed = structsforming(nullptr, buffer, readedcount, numofstrings_readed, &numofstrings_computed);
+
+    #endif
+
+
+    #ifdef NDEBUG
+
     if(strings_adressed == nullptr){
         fprintf(logfile, "LINE %d:  (!) strings_adressed is nullptr!\n", __LINE__);
         return -1;
@@ -34,19 +51,35 @@ int main(){
 
     fprintf(logfile, "LINE %d:  numofstrings_computed is %d\n", __LINE__, numofstrings_computed);
 
+    #endif
+
     //Работа:
         //Сортировка:
     qsort(strings_adressed, numofstrings_computed + 1, sizeof(strinfo),(int(*) (const void *, const void *)) compar);
     
+    #ifdef NDEBUG
+
     fprintf(logfile, "LINE %d:  After the qsort\n\n", __LINE__);
 
     for(int i = 0; i <= numofstrings_computed; i++) fprintf(logfile, "\nLINE %d:  length is %d,  strings_adressed[%d].adress is: %s ",__LINE__, strings_adressed[i].length, i, strings_adressed[i].adress);
     
     //Вывод: 
     fprintf(logfile, "\nLINE %d:  numofstring_computed = %d\n", __LINE__, numofstrings_computed);
+
     outputinfile(logfile, filewritename, strings_adressed, numofstrings_computed);
 
+    #else
+
+    outputinfile(nullptr, filewritename, strings_adressed, numofstrings_computed);
+
+    #endif
+    
+    #ifdef NDEBUG
+
     fclose(logfile);
+
+    #endif
+
     return 0;
 }
 
