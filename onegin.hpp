f3e2022c0a9e4e4313a6b87cd4e7b37d1b8c14dc
xmickos <91094084+xmickos@users.noreply.github.com>
2022-09-29 22:38:14 +0300

    Update onegin.hpp
    goto oneginfuncs commit

--- onegin.hpp
+++ onegin.hpp
@@ -1,21 +1,26 @@
+#ifndef ONEGIN_HPP
+#define ONEGIN_HPP
+
 #include <stdio.h>
 #include <string.h>
-#include <math.h>
-#include <assert.h>
 #include <malloc.h> 
 #include <stat.h>
 #include <stdlib.h>
 #include <ctype.h>
 
+// #define NDEBUG
+
 struct strinfo{
     char* adress = nullptr;
-    int length = -1;
+    int length   = -1;
 };
 
 int compar(strinfo* str1, strinfo* str2);
 
-void readingfromfile(FILE* logfile, char* filereadname, char** buffer, long int* readedcount, int *numofstrings_readed);
+char* readingfromfile(FILE* logfile, char* filereadname, long int* readedcount, int *numofstrings_readed);
 
-strinfo* structsforming(FILE* logfile, char** buffer, long int readedcount, int numofstrings_readed, int* numofstrings_computed);
+strinfo* structsforming(FILE* logfile, char* buffer, long int readedcount, int numofstrings_readed, int* numofstrings_computed);
 
 void outputinfile(FILE* logfile, char* filewritename, strinfo* strings_adressed, int numofstrings_computed);
+
+#endif // ONEGIN_HPP
