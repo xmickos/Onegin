7011473c2631ba5241d114c636082d3e6aff27da
xmickos <91094084+xmickos@users.noreply.github.com>
2022-10-03 12:14:26 +0300

    Backwards sort + final output.
    Added backwards sort func (new compar – backwardscompar) + fixed outputinfile() func for the new output.

--- onegin.hpp
+++ onegin.hpp
@@ -15,12 +15,16 @@ struct strinfo{
     int length   = -1;
 };
 
-int compar(strinfo* str1, strinfo* str2);
+int forwardscompar(strinfo* str1, strinfo* str2);
 
 char* readingfromfile(FILE* logfile, char* filereadname, long int* readedcount, int *numofstrings_readed);
 
 strinfo* structsforming(FILE* logfile, char* buffer, long int readedcount, int numofstrings_readed, int* numofstrings_computed);
 
 void outputinfile(FILE* logfile, char* filewritename, strinfo* strings_adressed, int numofstrings_computed);
+
+int backwardscompar(strinfo* str1, strinfo* str2);
+
+void outputinfile(FILE* logfile, char* filewritename, strinfo* strings_adressed, int numofstrings_computed, int sort_direction, char* buffer);
 
 #endif // ONEGIN_HPP
